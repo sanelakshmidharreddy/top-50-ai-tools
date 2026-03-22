0e6ee584e2272a8b1f54ecd5c174e783cd739eb0
sanelakshmidharreddy <sanelakshmidharreddy@gmail.com>
2026-03-22 14:30:35 +0530

    Create constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const AI_TOOLS_LIST = [
+  "Replit Ghostwriter", "Uizard", "Durable", "HeyGen", "Descript", "Microsoft Power BI", "Obviously AI",
+  "ChatGPT", "Perplexity AI", "Copyleaks AI", "Consensus", "Gamma", "Visme", "Notion AI", "Taskade",
+  "GitHub Copilot", "Mutable AI", "MATLAB", "Luma AI", "Scenario", "Pika", "Canva Magic Studio",
+  "Builder.ai", "Beautiful.ai", "MonkeyLearn", "V0.dev", "Figma AI", "Kaedim3D", "Khroma", "NotebookLM",
+  "Synthesia.io", "Runway ML", "Animoto", "Akkio", "Elicit.org", "Scite.ai", "Claude.ai", "Wisdolia",
+  "Blackbox AI", "Mendix", "Tome.app", "Decktopus", "GrammarlyGO", "ClickUp Brain", "Leonardo.ai",
+  "Writesonic", "AppyPie AI", "Codeium", "Tableau GPT", "DeepBrain AI"
+];
